2ee7aa1fbff31b907aeba6788aab7c5666bfcca3
Wendi Chen <epanistamai.proles@gmail.com>
2018-11-22 18:30:35 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,35 +1,41 @@
 def create_an_empty_array
-  
+  empty_array = []
+  empty_array
 end
 
 def create_an_array
-  
+  new_array = ["katara", "toph", "aang", "zuko"]
+  new_array
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  array << element
+  array
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  array.unpush(element)
+  array
 end
 
 def remove_element_from_end_of_array(array)
-  
+  array.pop
+  array
 end
 
 def remove_element_from_start_of_array(array)
-  
+  array.push
+  array
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+  array[index number]
 end
 
 def retrieve_first_element_from_array(array)
-  
+  array[0]
 end
 
 def retrieve_last_element_from_array(array)
-  
+  array[-1]
 end
